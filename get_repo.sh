d8b859b82ddcb74de1c0f9286640eaaa6ab44b65
chaNcharge <chancharge@gmail.com>
2019-02-22 12:04:01 -0800

    Use latest builds

--- get_repo.sh
+++ get_repo.sh
@@ -8,7 +8,4 @@ else
   cd vscode
 fi
 
-export LATEST_MS_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
-echo "Got the latest MS tag: ${LATEST_MS_TAG}"
-git checkout $LATEST_MS_TAG
 cd ..
